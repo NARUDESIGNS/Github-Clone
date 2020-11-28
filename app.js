a7d7e91d3e8a8e5a4c4a065c7f21b6a0b536f307
Naru Designs <ibeabuchipaul@gmail.com>
2020-11-29 00:49:07 +0100

    UI stable for now

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
-//Resources
+//DOM Elements
+const nav = document.getElementById('nav');
+
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-nav-menu');
+
+const navNewBtn = document.getElementById('nav-new-btn');
+const newBtnDropdown = document.getElementById('new-btn-modal');
+
+const navAvatarBtn = document.getElementById('nav-avatar-btn');
+const navAvatarBtnDropdown = document.getElementById('avatar-btn-modal');
+
+const mainNavAvatar = document.getElementById('main-nav-avatar');
+const mainNav = document.getElementById('main-nav');
+
+const findRepoInput = document.getElementById('find-repo-input');
+
+const overallReposContainer = document.getElementById('main-body-repos'); 
 
 
+//------------------------------------------- UI PROCESSING -------------------------------------------
+//Nav menu is clicked
+menuBtn.addEventListener('click', () => {
+    //mobileMenu.style.display = "block";
+    mobileMenu.classList.toggle('show');
+})
+
+//New Btn '+' symbol is clicked - close other visible modals and show new btn modal
+navNewBtn.addEventListener('click', () => {
+    newBtnDropdown.classList.toggle('show');
+    navAvatarBtnDropdown.classList.remove('show');
+})
+
+//Avatart Btn is clicked - close other visible modals and show avatar btn modal
+navAvatarBtn.addEventListener('click', () => {
+    navAvatarBtnDropdown.classList.toggle('show');
+    newBtnDropdown.classList.remove('show');
+})
+
+//Close all modals and dropdowns when user clicks on the page
+document.querySelector('.main-body').addEventListener('click', () => {
+    newBtnDropdown.classList.contains('show') ? newBtnDropdown.classList.remove('show') : '';
+    navAvatarBtnDropdown.classList.contains('show') ? navAvatarBtnDropdown.classList.remove('show') : '';
+})
+
+//display main nav avatar on a particular scroll height
+window.addEventListener('scroll', (e) => {
+    window.scrollY > 340 ? mainNavAvatar.classList.add('visible') : mainNavAvatar.classList.remove('visible');
+})
+
+//readjust main nav for non-desktop screens
+if(window.outerWidth > 801){
+    nav.insertAdjacentElement('afterend', mainNav);
+}
+else if(window.outerWidth < 801) {
+    findRepoInput.insertAdjacentElement('beforebegin', mainNav);
+}
 
 
 
@@ -41,7 +95,7 @@ const repoQuery = {
                             }
                             url
                         }
-                    }
+                    } 
 
                     languages(orderBy: {direction: DESC, field: SIZE}, first: 1) {
                         nodes {
@@ -62,8 +116,6 @@ const repoQuery = {
 }
 
 
-
-
 //Fetch API function
 function getData(query){
     fetch(url, {
@@ -75,16 +127,13 @@ function getData(query){
         body: JSON.stringify(query)
     })
     .then(response => response.json())
-    .then(data => console.log(data.data.user))
+    .then(data => {
+        console.log(data.data.user);
+        return data;
+    })
     .catch(error => {
         console.log(error.message); 
     })
 } 
 
-x = `query { 
-    user (login: "narudesigns"){
-        name
-    }
-}`
-getData(repoQuery);   
-//JSON.stringify(query) 
+getData(repoQuery);
